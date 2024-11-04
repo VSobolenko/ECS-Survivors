14a207449e91eff01e6e7985246737bcafc722e8
VSobolenko <v.sobolenko.vs@gmail.com>
2024-11-04 23:45:57 +0300

    unity setup

---
+++ unity_ecs-survivors/Assets/TestScript.cs
@@ -0,0 +1,22 @@
+﻿using Entitas;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+public class Id : IComponent
+{
+    public int Value;
+}
+
+public class WorldPosition : IComponent
+{
+    public Vector3 Value;
+}
+class CheckImprovements
+{
+    public CheckImprovements()
+    {
+        GameEntity entity = null;
+    }
+}
+}
